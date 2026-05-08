c47bab1685d9906de95248df0d4204481dd885c8
Hunter Jordan <jordan.hunter0508@gmail.com>
2026-05-07 23:44:23 -0500

    Added a sample card to test with no camera

--- main.py
+++ main.py
@@ -3,37 +3,59 @@ from camera.usb_camera import UsbCamera
 
 usbCamera = UsbCamera()
 
-try:
+# When a camera is connected
+# try:
 
-    camera = usbCamera.connect_carmera(0)
-    print("Camera Connected")
+#     camera = usbCamera.connect_carmera(0)
+#     print("Camera Connected")
 
-    while(True):
-        raw_image = usbCamera.capture_image(camera)
-        prepared_image = usbCamera.prepare_image(raw_image)
-        points = usbCamera.find_card(prepared_image)
+#     while(True):
+#         raw_image = usbCamera.capture_image(camera)
+#         prepared_image = usbCamera.prepare_image(raw_image)
+#         points = usbCamera.find_card(prepared_image)
 
-        if points is not None:
-            points = usbCamera.order_points(points)
-            print(f"Ordered points: {points}")
+#         if points is not None:
+#             points = usbCamera.order_points(points)
+#             print(f"Ordered points: {points}")
 
-            card = usbCamera.get_card(raw_image, points)
+#             card = usbCamera.get_card(raw_image, points)
 
-            card = cv2.resize(card, (366,512))
-            cv2.imshow("Card", card)
+#             card = cv2.resize(card, (366,512))
+#             cv2.imshow("Card", card)
         
-        cv2.imshow("Live Stream",raw_image)
+#         cv2.imshow("Live Stream",raw_image)
 
-        if cv2.waitKey(1) == ord('q'):
-            break
+#         if cv2.waitKey(1) == ord('q'):
+#             break
     
-    print("Card")
+#     print("Card")
 
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+# except Exception as e:
+#     print("An error occured: ", e)
+
+
+
+
+
+# If no camera is connected 
+try:
+    raw_image = cv2.imread("sample_card.jpg")
+    prepared_image = usbCamera.prepare_image(raw_image)
+    points = usbCamera.find_card(prepared_image)
+
+    if points is not None:
+        points = usbCamera.order_points(points)
+        print(f"Ordered points: {points}")
+
+        card = usbCamera.get_card(raw_image, points)
+
+        card = cv2.resize(card, (366,512))
+        cv2.imshow("Card", card)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
     
-    
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
 except Exception as e:
     print("An error occured: ", e)
-
-
+    
